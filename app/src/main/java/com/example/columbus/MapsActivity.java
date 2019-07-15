c95b0e7a5be71becf88ee7b983d50bb491ab6e8e
mayuTakaji <tmayu228@gmail.com>
2019-07-15 23:44:08 +0900

    [UPDATE]permission確認

--- app/src/main/java/com/example/columbus/MapsActivity.java
+++ app/src/main/java/com/example/columbus/MapsActivity.java
@@ -1,27 +1,31 @@
 package com.example.columbus;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.FragmentActivity;
-import android.content.res.Resources;
-import android.util.Log;
 
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, LocationListener {
 
     private static final int MY_LOCATION_REQUEST_CODE = 0;
     private LocationManager myLocationManager;
@@ -30,7 +34,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -48,69 +51,122 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
 
-    @Override
-    //パーミッションを許可するかを聞いた結果をとるやつ
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        if (requestCode == MY_LOCATION_REQUEST_CODE) {
-            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //許可した
-            }
-        }
-    }
-
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     //マップ描画
     public void onMapReady(GoogleMap googleMap) {
+        /*try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("なんかできないよ", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("原因不明だよ", "Can't find style. Error: ", e);
+        }*/
+
         mMap = googleMap;
-
-        //パーミッション許可のチェック
-        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("位置情報が必要です。");
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //==== 承認要求 ====//
-                        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
-                    }
-                });
-                builder.setNegativeButton("拒否", null);
-                builder.show();
+        //permissionチェック
+        if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            myLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            String provider = getProvider();
+            Location lastLocation = myLocationManager.getLastKnownLocation(provider);
+            if(lastLocation != null) {
+                setLocation(lastLocation);
             }
-            String[] permissons = new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            };
-            ActivityCompat.requestPermissions(MapsActivity.this, permissons, MY_LOCATION_REQUEST_CODE);
-
-            try {
-                // Customise the styling of the base map using a JSON object defined
-                // in a raw resource file.
-                boolean success = googleMap.setMapStyle(
-                        MapStyleOptions.loadRawResourceStyle(
-                                this, R.raw.style_json));
-
-                if (!success) {
-                    Log.e("なんかできないよ", "Style parsing failed.");
-                }
-            } catch (Resources.NotFoundException e) {
-                Log.e("原因不明だよ" , "Can't find style. Error: ", e);
-            }
+            mMap.setMyLocationEnabled(true);
+            Toast.makeText(this, "Provider=" + provider, Toast.LENGTH_SHORT).show();
+            myLocationManager.requestLocationUpdates(provider, 0, 0, this);
+        } else {
+            setDefaultLocation();
+            confirmPermission();
         }
-
-        //mapに現在地マーカーを表示
-        mMap.setMyLocationEnabled(true);
-        myLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = myLocationManager.getBestProvider(criteria, true);
-        myLocationManager.requestLocationUpdates(provider,0,0, (LocationListener) this);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+                myLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                myLocationManager.requestLocationUpdates(getProvider(), 0, 0, this);
+            } else {
+                Toast.makeText(this, getString(R.string.permission_location_denied), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "LocationChanged実行" , Toast.LENGTH_SHORT).show();
+        setLocation(location);
+        try {
+            myLocationManager.removeUpdates(this);
+        } catch(SecurityException e) {
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            myLocationManager.removeUpdates(this);
+        } catch(SecurityException e) {
+        }
+    }
+
+    private String getProvider() {
+        Criteria criteria = new Criteria();
+        return myLocationManager.getBestProvider(criteria, true);
+    }
+
+    private void confirmPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.permission_location_rationale_title)
+                    .setMessage(R.string.permission_location_rationale)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MapsActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    MY_LOCATION_REQUEST_CODE);
+                        }
+                    })
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
+        }
+    }
+
+    private void setDefaultLocation() {
+        LatLng tokyo = new LatLng(35.681298, 139.766247);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo, 18));
+    }
+
+    private void setLocation(Location location) {
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("now Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 18));
+    }
 }
